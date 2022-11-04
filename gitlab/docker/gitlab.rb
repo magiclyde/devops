7a4307afc61563ef0e32dc0634e75c94880715bb
clyde <magiclyde@163.com>
2022-11-04 17:45:55 +0800

    install gitlab with docker

---
+++ gitlab/docker/gitlab.rb
@@ -0,0 +1,15 @@
+external_url 'http://mycode.com'
+gitlab_rails['time_zone'] = 'Asia/Shanghai'
+gitlab_rails['gitlab_shell_ssh_port'] = 8022
+=begin
+gitlab_rails['smtp_enable'] = true
+gitlab_rails['smtp_address'] = "smtp.exmail.qq.com"
+gitlab_rails['smtp_port'] = 465
+gitlab_rails['smtp_user_name'] = "changeme"
+gitlab_rails['smtp_password'] = "changeme"
+gitlab_rails['smtp_domain'] = "exmail.qq.com"
+gitlab_rails['smtp_authentication'] = "login"
+gitlab_rails['smtp_enable_starttls_auto'] = true
+gitlab_rails['smtp_tls'] = true
+gitlab_rails['gitlab_email_from'] = 'changeme'
+=end
