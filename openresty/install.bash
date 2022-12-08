f104f2e8cd7e7941a48b40ec601c3aa771f3d459
clyde <magiclyde@163.com>
2022-12-08 16:13:53 +0800

    add-module ngx_http_proxy_connect_module

--- openresty/install.bash
+++ openresty/install.bash
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
+# https://segmentfault.com/a/1190000040325948/en
+# https://github.com/chobits/ngx_http_proxy_connect_module
 
 cd /tmp
-export VERSION=1.19.3.1
+
+VERSION=1.19.3.1
+BUNDLE_VERSION=${VERSION:0:${#VERSION}-2}
+
+git clone https://github.com/chobits/ngx_http_proxy_connect_module.git
+
 wget https://openresty.org/download/openresty-$VERSION.tar.gz
 tar -xzvf openresty-$VERSION.tar.gz
-cd openresty-$VERSION/
-./configure --with-http_stub_status_module --with-http_v2_module
+
+cd /tmp/openresty-$VERSION/bundle/nginx-$BUNDLE_VERSION
+patch -p1 < /tmp/ngx_http_proxy_connect_module/patch/proxy_connect_rewrite_1018.patch
+
+cd /tmp/openresty-$VERSION/
+./configure --with-http_stub_status_module --with-http_v2_module --add-module=/tmp/ngx_http_proxy_connect_module
 make
-sudo make install
-/usr/local/openresty/bin/openresty -v
+/tmp/openresty-1.19.3.1/build/nginx-1.19.3/objs/nginx -V
+#sudo make install
+
